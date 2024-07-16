b9ad70f79c778863f9c507d98925f04de64a581d
io_oti <io_oti@outlook.com>
2024-07-16 12:29:00 +0800

    chore: Modify the sidebar and ignore the draft

--- docs/.vitepress/configs/sidebar.js
+++ docs/.vitepress/configs/sidebar.js
@@ -1,8 +1,9 @@
-import { readFileSync, readdirSync, statSync } from 'fs'
-import { basename, extname } from 'path'
+import { readFileSync, readdirSync, statSync } from "fs"
+import { basename, extname } from "path"
+import matter from "gray-matter"
 
-const BASE_PATH = './docs'
-const EXT_NAME = '.md'
+const BASE_PATH = "./docs"
+const EXT_NAME = ".md"
 
 // 判断是否是文件夹
 const isDirectory = (path) => statSync(path).isDirectory()
@@ -17,30 +18,25 @@ const readDirectory = (path) => {
 // 重置 index.md 位置
 const resetIndex = (items) => {
   return items.reduce((res, item, idx) => {
-    return item === 'index.md' ? [...res.splice(idx, 1), ...res] : res
+    return item === "index.md" ? [...res.splice(idx, 1), ...res] : res
   }, items)
 }
 
 // 首字母转大写
 const firstToUpperCase = (str) => {
-  return typeof str === 'string'
+  return typeof str === "string"
     ? str.replace(/^(\w{1})/, (s) => s.toUpperCase())
-    : ''
+    : ""
 }
 
 // 判断是否是 markdown 文件
 const isMarkdown = (filename) => extname(filename) === EXT_NAME
 
 // 读取文件
-const readFile = (path) => readFileSync(path, { encoding: 'utf-8' })
+const readFile = (path) => readFileSync(path, "utf-8")
 
-// 获取标题
-const getTitle = (path) => {
-  const content = readFile(path);
-  const titles = content.match(/[#](.*)/g);
-
-  return titles !== null ? titles[0].replace(/\#+/g, "") : ''
-}
+// 获取 frontmatter
+const getFrontmatter = (path) => matter(readFile(path))
 
 // 递归文件夹中读取文件
 const recursionDir = (path) => {
@@ -56,9 +52,15 @@ const recursionDir = (path) => {
     }
 
     if (isMarkdown(item)) {
+      const {
+        data: { draft, title },
+      } = getFrontmatter(`${prevPath}/${item}`)
+
+      if (draft) return res
+
       res.push({
-        text: getTitle(`${prevPath}/${item}`),
-        link: `${path}/${item}`
+        text: title,
+        link: `${path}/${item}`,
       })
     }
 
@@ -66,7 +68,7 @@ const recursionDir = (path) => {
   }, [])
 }
 
-export function genSidebar (...paths) {
+export function genSidebar(...paths) {
   return paths.reduce((res, path) => {
     res[`/${path}/`] = { items: recursionDir(path) }
 
