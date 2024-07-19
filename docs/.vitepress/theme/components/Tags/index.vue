64b6f2ccca12427fe2fa4750f95f9f9de9c14f2a
io_oti <io_oti@outlook.com>
2024-07-19 17:37:36 +0800

    feat: Added tag component

---
+++ docs/.vitepress/theme/components/Tags/index.vue
@@ -0,0 +1,41 @@
+<script lang="jsx">
+import { ref } from "vue"
+
+export default {
+  props: {
+    tags: {
+      type: String,
+      default: "Tag",
+    },
+  },
+  setup(props, context) {
+    const tags = props.tags.split(",")
+
+    return () => (
+      <>
+        {tags.map((tag) => (
+          <span class="tag">{tag}</span>
+        ))}
+      </>
+    )
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.tag {
+  padding: 4px 8px;
+  font-size: 12px;
+  line-height: 16px;
+  border-radius: 4px;
+  background: var(--vp-c-bg-soft);
+
+  &-blue {
+    background-color: var(--color-blue);
+  }
+
+  span + & {
+    margin-left: 12px;
+  }
+}
+</style>
